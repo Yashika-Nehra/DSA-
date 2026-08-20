9bd61f3b7e4cae17f4d781eb09e24dbe978fa332
Yashika-Nehra <nehrayashika767@gmail.com>
2026-08-20 09:16:09 +0530

    added implementation of union

---
+++ two pointers/union.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int unionArrays(int A[], int m, int B[], int n, int C[]){
+    int i = 0, j = 0, k = 0;
+    while (i < m && j < n) {
+        if (A[i] < B[j]) {
+            C[k++] = A[i++];
+        }
+        else if (A[i] > B[j]) {
+            C[k++] = B[j++];
+        }
+        else {
+            C[k++] = A[i++];
+            j++;
+        }
+    }
+    while (i < m) {
+        C[k++] = A[i++];
+    }
+    while (j < n) {
+        C[k++] = B[j++];
+    }
+    return k;
+}
+
+int main(){
+    int m, n;
+    cout << "Enter the size of first array: ";
+    cin >> m;
+    int A[m];
+    cout << "Enter the elements of first array: ";
+    for (int i = 0; i < m; i++) {
+        cin >> A[i];
+    }
+    cout << "Enter the size of second array: ";
+    cin >> n;
+    int B[n];
+    cout << "Enter the elements of second array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
+    int C[m + n];
+    int size = unionArrays(A, m, B, n, C);
+    cout << "Union of the two arrays is: ";
+    for (int i = 0; i < size; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
