8df7e8dfa1d17e1b24c69188c959de989fd9a19e
Yashika-Nehra <nehrayashika767@gmail.com>
2026-09-15 23:14:32 +0530

    Add counting of leaf

---
+++ tree/count_leaf.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int val;
+    node *left;
+    node *right;
+
+    node(int data){
+        val=data;
+        left=right=NULL;
+    }
+};
+
+int countLeafNodes(node *root){
+    if(root==NULL)
+    return 0;
+
+    if(root->left==NULL && root->right==NULL)
+    return 1;
+
+    return countLeafNodes(root->left)+countLeafNodes(root->right);
+}
+
+int main(){
+    node *root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+    root->left->left=new node(4);
+    root->left->right=new node(5);
+    root->right->right=new node(6);
+
+    cout<<"The count of leaf nodes="<<countLeafNodes(root);
+}
