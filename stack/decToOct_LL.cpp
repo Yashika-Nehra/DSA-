4ac3701a78a13a557b254895f0f0ad54d09f6c8c
Yashika-Nehra <nehrayashika767@gmail.com>
2026-09-17 10:25:28 +0530

    Add decimal to octal conversion using stack

---
+++ stack/decToOct_LL.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Stack {
+    Node* top;
+
+public:
+    Stack() {
+        top = NULL;
+    }
+
+    void push(int x) {
+        Node* newNode = new Node(x);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        int x = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return x;
+    }
+
+    bool isEmpty() {
+        return top == NULL;
+    }
+};
+
+int main() {
+    int n;
+
+    cout << "Enter decimal number: ";
+    cin >> n;
+
+    Stack s;
+
+    if (n == 0) {
+        cout << "Octal = 0";
+        return 0;
+    }
+
+    while (n > 0) {
+        s.push(n % 8);
+        n = n / 8;
+    }
+
+    cout << "Octal = ";
+
+    while (!s.isEmpty()) {
+        cout << s.pop();
+    }
+
+    return 0;
+}
